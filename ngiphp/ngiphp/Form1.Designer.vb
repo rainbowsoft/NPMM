99b679ce3eec19e2c906fb1f071f47914be6fa3a
rainbowsoft <rainbowsoft@gmail.com>
2014-12-09 18:42:45 +0800

    提交；

---
+++ ngiphp/ngiphp/Form1.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.退出ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(336, 64)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "启动"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(336, 165)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "启动"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("宋体", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(20, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(185, 54)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("宋体", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(20, 109)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(185, 54)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("宋体", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(20, 214)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(185, 54)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Label3"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(336, 272)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "启动"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(29, 66)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(301, 21)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.Text = "d:\nginx\"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(29, 167)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(301, 21)
+        Me.TextBox2.TabIndex = 7
+        Me.TextBox2.Text = "d:\nginx\php52\"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(29, 274)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(301, 21)
+        Me.TextBox3.TabIndex = 8
+        Me.TextBox3.Text = "d:\xampp\mysql\bin\"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(29, 195)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(41, 16)
+        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Tag = "1"
+        Me.RadioButton1.Text = "5.2"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(76, 195)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(41, 16)
+        Me.RadioButton2.TabIndex = 10
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Tag = "1"
+        Me.RadioButton2.Text = "5.3"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(123, 194)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(41, 16)
+        Me.RadioButton3.TabIndex = 11
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Tag = "1"
+        Me.RadioButton3.Text = "5.4"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(170, 194)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(41, 16)
+        Me.RadioButton4.TabIndex = 12
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Tag = "1"
+        Me.RadioButton4.Text = "5.5"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(217, 194)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(41, 16)
+        Me.RadioButton5.TabIndex = 13
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Tag = "1"
+        Me.RadioButton5.Text = "5.6"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NPM管理器"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.退出ToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(101, 26)
+        '
+        '退出ToolStripMenuItem
+        '
+        Me.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem"
+        Me.退出ToolStripMenuItem.Size = New System.Drawing.Size(100, 22)
+        Me.退出ToolStripMenuItem.Text = "退出"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(444, 326)
+        Me.Controls.Add(Me.RadioButton5)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Nginx+PHP+MySQL"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton5 As System.Windows.Forms.RadioButton
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents 退出ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ ngiphp/ngiphp/Form1.vb
@@ -0,0 +1,188 @@
+﻿Public Class Form1
+
+
+    Dim n As Process = Nothing
+
+    Dim p As Process = Nothing
+
+    Dim m As Process = Nothing
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If IsNothing(n) = True Then
+            n = New Process
+            n.StartInfo.FileName = "nginx.exe"
+            n.StartInfo.WorkingDirectory = TextBox1.Text
+            n.StartInfo.CreateNoWindow = True
+            n.Start()
+            'n.WaitForExit()
+            Label1.Text = Label1.Text + n.Id.ToString
+            Label1.ForeColor = Color.Green
+
+
+        Else
+            Label1.ForeColor = Color.Red
+            Label1.Text = "Nginx: "
+            Process.Start("taskkill", "/im nginx.exe /f")
+            n = Nothing
+        End If
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If IsNothing(p) = True Then
+
+            p = New Process
+            p.StartInfo.FileName = "php-cgi.exe"
+            p.StartInfo.WorkingDirectory = TextBox2.Text
+            p.StartInfo.CreateNoWindow = True
+            p.StartInfo.Arguments = "-b 127.0.0.1:9000"
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+            p.Start()
+
+            Label2.Text = Label2.Text + p.Id.ToString
+            Label2.ForeColor = Color.Green
+        Else
+
+            Label2.ForeColor = Color.Red
+            Label2.Text = "PHP 5: "
+            Process.Start("taskkill.exe", "/im php-cgi.exe /f")
+            p.Kill()
+            p = Nothing
+        End If
+    End Sub
+
+
+
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        If IsNothing(n) = False Then Process.Start("taskkill.exe", "/im nginx.exe /f")
+        If IsNothing(p) = False Then Process.Start("taskkill.exe", "/im php-cgi.exe /f")
+        If IsNothing(m) = False Then Process.Start("taskkill.exe", "/im mysqld.exe /f")
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "Nginx: "
+        Label2.Text = "PHP 5: "
+        Label3.Text = "MySQL: "
+        'Select Case My.Settings.php
+        '    Case 52
+        '        RadioButton1.Select()
+        '    Case 53
+        '        RadioButton2.Select()
+        '    Case 54
+        '        RadioButton3.Select()
+        '    Case 55
+        '        RadioButton4.Select()
+        '    Case 56
+        '        RadioButton5.Select()
+        'End Select
+
+        Dim d = My.Computer.FileSystem.ReadAllText(My.Application.Info.DirectoryPath() + "\d.txt")
+
+        Dim p = My.Computer.FileSystem.ReadAllText(My.Application.Info.DirectoryPath() + "\p.txt")
+
+        Dim m = My.Computer.FileSystem.ReadAllText(My.Application.Info.DirectoryPath() + "\m.txt")
+
+        TextBox1.Text = d
+        TextBox2.Text = p
+        TextBox3.Text = m
+
+        If (p.Contains("php52") = True) Then RadioButton1.Select()
+        If (p.Contains("php53") = True) Then RadioButton2.Select()
+        If (p.Contains("php54") = True) Then RadioButton3.Select()
+        If (p.Contains("php55") = True) Then RadioButton4.Select()
+        If (p.Contains("php56") = True) Then RadioButton5.Select()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If IsNothing(m) = True Then
+
+            m = New Process
+            m.StartInfo.FileName = "mysqld.exe"
+            m.StartInfo.WorkingDirectory = TextBox3.Text
+            m.StartInfo.CreateNoWindow = True
+            m.StartInfo.Arguments = "--defaults-file=" + TextBox3.Text + "my.ini --standalone"
+            m.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+            m.Start()
+
+            Label3.Text = Label3.Text + m.Id.ToString
+            Label3.ForeColor = Color.Green
+        Else
+            Label3.ForeColor = Color.Red
+            Label3.Text = "MySQL: "
+            Process.Start("taskkill.exe", "/im php-cgi.exe /f")
+            m.Kill()
+            m = Nothing
+        End If
+    End Sub
+
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        'TextBox2.Text = "d:\nginx\php52\"
+        'My.Settings.php = 52
+        'My.Settings.Save()
+        Dim p = My.Computer.FileSystem.GetDirectoryInfo(My.Application.Info.DirectoryPath).Parent.Parent.Parent.Parent.FullName
+        p = p + "\php" + "52" + "\"
+        TextBox2.Text = p
+        My.Computer.FileSystem.WriteAllText(My.Application.Info.DirectoryPath() + "\p.txt", p, False)
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        'TextBox2.Text = "d:\nginx\php53\"
+        'My.Settings.php = 53
+        'My.Settings.Save()
+        Dim p = My.Computer.FileSystem.GetDirectoryInfo(My.Application.Info.DirectoryPath).Parent.Parent.Parent.Parent.FullName
+        p = p + "\php" + "53" + "\"
+        TextBox2.Text = p
+        My.Computer.FileSystem.WriteAllText(My.Application.Info.DirectoryPath() + "\p.txt", p, False)
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        'TextBox2.Text = "d:\nginx\php54\"
+        'My.Settings.php = 54
+        'My.Settings.Save()
+        Dim p = My.Computer.FileSystem.GetDirectoryInfo(My.Application.Info.DirectoryPath).Parent.Parent.Parent.Parent.FullName
+        p = p + "\php" + "54" + "\"
+        TextBox2.Text = p
+        My.Computer.FileSystem.WriteAllText(My.Application.Info.DirectoryPath() + "\p.txt", p, False)
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        'TextBox2.Text = "d:\nginx\php55\"
+        'My.Settings.php = 55
+        'My.Settings.Save()
+        Dim p = My.Computer.FileSystem.GetDirectoryInfo(My.Application.Info.DirectoryPath).Parent.Parent.Parent.Parent.FullName
+        p = p + "\php" + "55" + "\"
+        TextBox2.Text = p
+        My.Computer.FileSystem.WriteAllText(My.Application.Info.DirectoryPath() + "\p.txt", p, False)
+    End Sub
+
+    Private Sub RadioButton5_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton5.CheckedChanged
+        'TextBox2.Text = "d:\nginx\php56\"
+        'My.Settings.php = 56
+        'My.Settings.Save()
+        Dim p = My.Computer.FileSystem.GetDirectoryInfo(My.Application.Info.DirectoryPath).Parent.Parent.Parent.Parent.FullName
+        p = p + "\php" + "56" + "\"
+        TextBox2.Text = p
+        My.Computer.FileSystem.WriteAllText(My.Application.Info.DirectoryPath() + "\p.txt", p, False)
+    End Sub
+
+    Private Sub 退出ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 退出ToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+        End If
+
+
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+End Class
